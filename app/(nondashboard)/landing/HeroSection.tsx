94f1fd66ac65540898fe09ceeaea388a25ff2c17
kenzosashida090 <kenzosashida1@gmail.com>
2026-03-23 14:15:07 -0600

    [new] Add search engine

--- app/(nondashboard)/landing/HeroSection.tsx
+++ app/(nondashboard)/landing/HeroSection.tsx
@@ -1,11 +1,40 @@
 
 "use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import {motion} from "framer-motion"
 import { Input } from '../../../components/ui/input'
 import { Button } from '../../../components/ui/button'
+import { useRouter } from 'next/navigation'
+import { useDispatch } from 'react-redux'
+import { setFilters } from '../../../state'
 const HeroSection = () => {
+    const [inputSearch, setInputSearch] = useState('')
+    const dispatch = useDispatch()
+    const router = useRouter()
+    const onSubmitSearch = async()=>{
+        const searchTrim = inputSearch.trim()
+        if(!searchTrim ) return
+        try{
+            const response = await fetch(`https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(searchTrim)}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
+            const data = await response.json()
+            const [lng, lat] = data.features[0].geometry.coordinates
+            if(data.features && data.features.length > 0 ) {
+                dispatch(setFilters({
+                    location:searchTrim,
+                    coordinates:[lng,lat]
+                }))
+                const params = new URLSearchParams({
+                    location: searchTrim,
+                    lat: lat.toString(),
+                    lng:lng.toString()
+                })
+                router.push(`search/${params.toString()}`)
+            }
+        }catch(error){
+            console.log(error,'ddd')
+        }
+    }
   return (
     <div className='relative h-screen w-full'>
         <Image 
@@ -29,8 +58,8 @@ const HeroSection = () => {
                         <p className='text-xl text-white mb-8'>Explore our wide range of rental properties to fit your lifestyle and needs.</p>
                         <div className='flex justify-center '>
 
-                            <Input type='text' placeholder='Search by city, neighborhood or address' className='w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12 '/>  
-                            <Button className='bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12'>Search</Button>
+                            <Input onChange={(e)=>setInputSearch(e.target.value)} type='text' placeholder='Search by city, neighborhood or address' className='w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12 '/>  
+                            <Button onClick={onSubmitSearch} className='bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12'>Search</Button>
                         </div>
                     </div>
             </motion.div>
