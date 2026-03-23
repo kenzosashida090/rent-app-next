68ab0589a1264c4f887fd478673d2b9d329e60b7
kenzosashida090 <kenzosashida1@gmail.com>
2026-03-23 14:08:28 -0600

    [new] Adding aws s3 bucket service

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+{
+        protocol:'https',
+        hostname: `${process.env.S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com`,
+        pathname:"/**"
+      },
+      {
+        protocol:'https',
+        hostname: `example.com`,
+        pathname:"/**"
+      }
+    ]
+  }
 };
 
-export default nextConfig;
+
