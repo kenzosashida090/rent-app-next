c60b4dd7beb2756a65407dfa79f0d21c169b2db9
kenzosashida090 <kenzosashida1@gmail.com>
2026-03-23 21:33:55 -0600

    [fix[ remove un used var

--- app/(nondashboard)/search/Map.tsx
+++ app/(nondashboard)/search/Map.tsx
@@ -10,7 +10,6 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string
 const Map = () => {
     const mapContainerRef = useRef(null)
     const filters = useAppSelector((state)=>state.global.filters)
-    const isFiltersFullOpen = useAppSelector((state)=>state.global.isFiltersFullOpen)
     const {isLoading, data:properties, isError} =useGetPropertiesQuery(filters)
         const createPropertyMarker = (property: Property, map: mapboxgl.Map)=>{
       console.log(property,"---------")
